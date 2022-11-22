82c637e83af084650cda4bf7175aab3cab46e1a8
ILYA KUZNETSOV <ilya.igorevich.kuz@gmail.com>
2022-11-22 18:53:53 +0300

    refactor tests to contstants

--- kuznetchik/Tests/KuznetchikTest.cs
+++ kuznetchik/Tests/KuznetchikTest.cs
@@ -3,39 +3,29 @@
 [TestFixture]
 public class KuznetchikTest
 {
-    private const string RefKey = "8899AABBCCDDEEFF0011223344556677FEDCBA98765432100123456789ABCDEF";
-    private const string RefInput = "1122334455667700ffeeddccbbaa9988";
-    private const string RefOutput = "7f679d90bebc24305a468d42b9d4edcd";
-
     private Kuznetchik _kuznetchik;
-    private byte[] _key;
-    private byte[] _refInput;
-    private byte[] _refOutput;
 
     [SetUp]
     public void Setup()
     {
         _kuznetchik = new Kuznetchik();
-        _key = Converter.HexStringToByteArray(RefKey);
-        _refInput = Converter.HexStringToByteArray(RefInput);
-        _refOutput = Converter.HexStringToByteArray(RefOutput);
     }
     
     [Test]
     public void TestEncrypt()
     {
-        var encrypt = _kuznetchik.Encrypt(_refInput, _key);
+        var encrypt = _kuznetchik.Encrypt(ReferenceConstants.ByteInput, ReferenceConstants.ByteKey);
         var output = Converter.ByteArrayToString(encrypt);
 
-        Assert.AreEqual(RefOutput, output);
+        Assert.AreEqual(ReferenceConstants.Output, output);
     }
 
     [Test]
     public void TestDecrypt()
     {
-        var decrypt = _kuznetchik.Decrypt(_refOutput, _key);
+        var decrypt = _kuznetchik.Decrypt(ReferenceConstants.ByteOutput, ReferenceConstants.ByteKey);
         var output = Converter.ByteArrayToString(decrypt);
         
-        Assert.AreEqual(RefInput, output);
+        Assert.AreEqual(ReferenceConstants.Input, output);
     }
 }
