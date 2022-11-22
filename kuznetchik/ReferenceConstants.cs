da073128335f4d381998498e08e51ac093df5211
ILYA KUZNETSOV <ilya.igorevich.kuz@gmail.com>
2022-11-22 18:53:43 +0300

    Add test constants

---
+++ kuznetchik/ReferenceConstants.cs
@@ -0,0 +1,10 @@
+﻿public static class ReferenceConstants
+{
+    public const string Key = "8899AABBCCDDEEFF0011223344556677FEDCBA98765432100123456789ABCDEF";
+    public const string Input = "1122334455667700ffeeddccbbaa9988";
+    public const string Output = "7f679d90bebc24305a468d42b9d4edcd";
+
+    public static byte[] ByteKey => Converter.HexStringToByteArray(Key);
+    public static byte[] ByteInput => Converter.HexStringToByteArray(Input);
+    public static byte[] ByteOutput => Converter.HexStringToByteArray(Output);
+}
